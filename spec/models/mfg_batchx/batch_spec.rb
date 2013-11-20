9a7ab6238c12f18ae3288cc3b6caecc3f856357e
jc <emclab2011@gmail.com>
2013-11-20 13:33:11 -0600

    JC: added @erb_code and if loop in views.

--- spec/models/mfg_batchx/batch_spec.rb
+++ spec/models/mfg_batchx/batch_spec.rb
@@ -2,6 +2,34 @@ require 'spec_helper'
 
 module MfgBatchx
   describe Batch do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it "should be OK" do
+      c = FactoryGirl.create(:mfg_batchx_batch)
+      c.should be_valid
+    end
+    
+    it "should reject 0 order_id" do
+      c = FactoryGirl.build(:mfg_batchx_batch, :order_id => 0)
+      c.should_not be_valid
+    end
+    
+    it "should reject 0 rfq_id" do
+      c = FactoryGirl.build(:mfg_batchx_batch, :rfq_id => 0)
+      c.should_not be_valid
+    end
+    
+    it "should reject 0 qty" do
+      c = FactoryGirl.build(:mfg_batchx_batch, :qty => 0)
+      c.should_not be_valid
+    end
+    
+    it "should reject nil batch_status_id" do
+      c = FactoryGirl.build(:mfg_batchx_batch, :batch_status_id => nil)
+      c.should_not be_valid
+    end
+    
+    it "should reject nil start_date" do
+      c = FactoryGirl.build(:mfg_batchx_batch, :start_date => nil)
+      c.should_not be_valid
+    end
   end
 end
